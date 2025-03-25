85bec0fe13103e8d34a7da0098d71c281ce435a6
Tavini2002 <taviniperera2002@gmail.com>
2025-03-25 07:57:42 +0530

    pull requests from hima

--- frontend/src/Components/header.js
+++ frontend/src/Components/header.js
@@ -7,12 +7,12 @@ import logo from "../Images/Logo.png";
 function header(){
     return(
         <div className="headerbody">
-            <nav className="bg-transparent fixed top-0 left-0 w-full h-[15vh] pl-12 pr-10">
+            <nav className="bg-transparent bg-opacity-50 backdrop-blur-md fixed top-0 left-0 w-full h-[15vh] bg-[#5a250d] ">
 
-                <div className="max-w-7xl flex justify-between ">
+                <div className="max-w-7xl h-[15vh] flex justify-between ">
 
                     {/*---------Logo---------*/}
-                    <a href="/"><img src={logo} alt="" className="w-[10vw] max-w-xs mx-5 my-2" /></a>
+                    <a href="/"><img src={logo} alt="" className="w-[12vw] max-w-xs pl-8" /></a>
 
                     {/*----------Navigation Links----------------- */}
                     <div className="flex  space-x-20 text-white text-lg my-12">
@@ -20,7 +20,7 @@ function header(){
                         <a href="">STAY AWARE</a>
                         <a href="">QUICK REPORT</a>
                         <a href="">SECURE PASS</a>
-                        <a href="/riskScope">RISK SCOPE</a>
+                        <a href="">QUICK REPORT</a>
 
                     </div>
                 </div>
